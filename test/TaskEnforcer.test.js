5b1f783113eeab6ae7191152be3247c00d943228
Cinderbeast <105569378+Cinderbeast@users.noreply.github.com>
2022-08-28 17:23:57 -0600

    Update TaskEnforcer.test.js

--- test/TaskEnforcer.test.js
+++ test/TaskEnforcer.test.js
@@ -1,54 +1,19 @@
-const TeamLead = require("../lib/TeamLead");
+const Engineer = require("../lib/Taskenforcer");
 
-test("Can set GitHUb account via constructor", () => {
-  const testValue = "GitHubUser";
-  const e = new TeamLead("Foo", 1, "test@test.com", testValue);
-  expect(e.github).toBe(testValue);
+test("Can create a github.", () => {
+    const testGithub = "Cinderbeast";
+    const employeeInstance = new Engineer("Cindy", 3, "cindychynoweth@gmail.com", testGithub);
+    expect(employeeInstance.github).toBe(testGithub);
 });
 
-test('getRole() should return "TeamLead"', () => {
-  const testValue = "EnTeamLead";
-  const e = new TeamLead("Foo", 1, "test@test.com", "GitHubUser");
-  expect(e.getRole()).toBe(testValue);
+test("Testing getGithub will return github.", () => {
+    const testGithub = "Cinderbeast";
+    const employeeInstance = new Taskenforcer("Cindy", 3, "cindychynoweth@gmail.com", testGithub);
+    expect(employeeInstance.getGithub()).toBe(testGithub);
 });
 
-test("Can get GitHub username via getGithub()", () => {
-  const testValue = "GitHubUser";
-  const e = new TeamLead("Foo", 1, "test@test.com", testValue);
-  expect(e.getGithub()).toBe(testValue);
+test("Testing role.", () => {
+    const returnValue = "Taskenforcer";
+    const employeeInstance = new Taskenforcer("Cindy", 3, "cindychynoweth@gmail.com", "Cinderbeast");
+    expect(employeeInstance.getRole()).toBe(returnValue);
 });
-
-// const TaskEnforcer = require("../lib/TaskEnforcer");
-
-// test("Can create a github.", () => {
-//     const testGithub = "Sami";
-//     const employeeInstance = new TaskEnforcer("Sami", 2, "sami@work.com", testGithub);
-//     expect(employeeInstance.github).toBe(testGithub);
-// });
-
-// test("Testing getGithub will return github.", () => {
-//     const testGithub = "Cindy";
-//     const employeeInstance = new TaskEnforcer("Cindy", 3, "cindychynoweth@gmail.com", testGithub);
-//     expect(employeeInstance.getGithub()).toBe(testGithub);
-// });
-
-// test("Testing role.", () => {
-//     const returnValue = "TaskEnforcer";
-//     const employeeInstance = new TaskEnforcer("Rosie", 4, "rosie@work.com", testGithub);
-//     expect(employeeInstance.getRole()).toBe(returnValue);
-// });
-// test("Can create a github.", () => {
-//     const testGithub = "Emily";
-//     const employeeInstance = new TaskEnforcer("Emily", 5, "emily@work.com", testGithub);
-//     expect(employeeInstance.github).toBe(testGithub);
-// });
-// test("Can create a github.", () => {
-//     const testGithub = "Kaylee";
-//     const employeeInstance = new TaskEnforcer("Kaylee", 6, "kaylee@work.com", testGithub);
-//     expect(employeeInstance.github).toBe(testGithub);
-// });
-// test("Can create a github.", () => {
-//     const testGithub = "Amy";
-//     const employeeInstance = new TaskEnforcer("Amy", 7, "amy@work.com", testGithub);
-//     expect(employeeInstance.github).toBe(testGithub);
-// });
