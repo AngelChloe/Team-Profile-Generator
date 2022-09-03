cf4d38a8d18f04519c8f68c099fc595e3602c4f6
Cinderbeast <105569378+Cinderbeast@users.noreply.github.com>
2022-09-03 14:10:08 -0600

    Updated code

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -22,29 +22,3 @@ test("Testing email.", () => {
     const employeeInstance = new Employee("Cindy", 3, email);
     expect(employeeInstance.email).toBe(email);
 })
-
-
-
-test("Gets name through getName method.", () => {
-    const testName = "Cindy";
-    const employeeInstance = new Employee(testName);
-    expect(employeeInstance.getName()).toBe(testName);
-})
-
-test("Can test ID through getID method.", () => {
-    const testID = 2;
-    const employeeInstance = new Employee("Cindy", testID);
-    expect(employeeInstance.getId()).toBe(testID);
-})
-
-test("Can test email through getEmail method.", () => {
-    const testEmail = "cindychynoweths@gmail.com";
-    const employeeInstance = new Employee("Cindy", 3, testEmail);
-    expect(employeeInstance.getEmail()).toBe(testEmail);
-})
-
-test("Testing role.", () => {
-    const returnValue = "Employee";
-    const employeeInstance = new Employee("Cindy", 3, "cindychynoweth@gmail.com");
-    expect(employeeInstance.getRole()).toBe(returnValue);
-})
