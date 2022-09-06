f6908c8b068e882d0b750fc3676b9d9d9ec47435
Cinderbeast <105569378+Cinderbeast@users.noreply.github.com>
2022-09-05 18:12:50 -0600

    Made to pass

--- test/TeamLead.test.js
+++ test/TeamLead.test.js
@@ -2,19 +2,19 @@ const Teamlead = require("../lib/Teamlead");
 
 test("Can create an office number.", () => {
     const testOfficeNumber = 1;
-    const employeeInstance = new Teamlead("Cindy", 3, "cindychynoweth@gmail.com@gmail.com", testOfficeNumber);
+    const employeeInstance = new Teamlead("Gracie", 1, "cindychynoweth@gmail.com@gmail.com", testOfficeNumber);
     expect(employeeInstance.officeNumber).toBe(testOfficeNumber);
 });
 
 test("Testing officeNumber will return office number.", () => {
-    const testOfficeNumber = 3;
-    const employeeInstance = new Teamlead("Cindy", 3, "cindychynoweth@gmail.com", testOfficeNumber);
+    const testOfficeNumber = 1;
+    const employeeInstance = new Teamlead("Gracie", 1, "gracie@work", testOfficeNumber);
     expect(employeeInstance.getOfficeNumber()).toBe(testOfficeNumber);
 });
 
 test("Testing role.", () => {
     const returnValue = "Teamlead";
-    const employeeInstance = new Teamlead("Cindy", 3, "cindychynoweth@gmail.com", 2);
+    const employeeInstance = new Teamlead("Gracie", 1, "gracie@work.com", 2);
     expect(employeeInstance.getRole()).toBe(returnValue);
 });
 
